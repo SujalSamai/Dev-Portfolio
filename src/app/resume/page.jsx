3fbef01a9f9eed893cd44c36e1a9be0ab7e0947d
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-09-12 17:53:13 +0530

    Changed Resume links

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
         <div className="flex flex-col lg:flex-row w-11/12 lg:w-10/12 mx-auto">
           <Image src={resumeImg} width={500} height={500} className="shadow-lg px-4"/>
           <aside className="flex flex-col content-center justify-center gap-5 mt-10 lg:mt-0 w-[60%] lg:w-auto mx-auto">
-            <button className="flex items-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300" onClick={()=> window.open('https://drive.google.com/file/d/1xlI0F1IeoY3jSNJcU9nuidJINdYW91KE/view?usp=sharing')}>Resume on Drive <span className='animate-pulse'><BsArrowUpRight/></span></button>
+            <button className="flex items-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300" onClick={()=> window.open('https://docs.google.com/document/d/1BO9Kog7_RBK9es3TXP6lgrhnQ03dJeE131KPui6_vZA/edit?usp=sharing')}>Resume on Drive <span className='animate-pulse'><BsArrowUpRight/></span></button>
             <Link href="/resume.pdf" className="flex items-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300">Resume as PDF <span className='animate-pulse'><BsArrowUpRight/></span></Link>
             <Link href="https://indigo-ring-f16.notion.site/Sujal-Samai-b6d2c9e8d513441b9c0cc863d69d3705?pvs=4" className="flex items-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300">Notion Resume <span className='animate-pulse'><BsArrowUpRight/></span></Link>
           </aside>
