704195cf9997e2443df18493bfd129a4838e6e98
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-11-28 11:10:04 +0530

    Update Notion Link

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -19,14 +19,14 @@ const Resume = () => {
           <aside className="hidden md:flex flex-col content-center justify-center gap-3 md:gap-5 mt-10 lg:mt-0 w-[60%] lg:w-auto mx-auto">
             <button className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300" onClick={()=> window.open('https://docs.google.com/document/d/1BO9Kog7_RBK9es3TXP6lgrhnQ03dJeE131KPui6_vZA/edit?usp=sharing')}>Resume on Drive <span className='animate-pulse'><BsArrowUpRight/></span></button>
             <Link href="/resume.pdf" className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300">Resume as PDF <span className='animate-pulse'><BsArrowUpRight/></span></Link>
-            <Link href="https://indigo-ring-f16.notion.site/Sujal-Samai-b6d2c9e8d513441b9c0cc863d69d3705?pvs=4" className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300">Notion Resume <span className='animate-pulse'><BsArrowUpRight/></span></Link>
+            <Link href="https://sujalsamai.notion.site/Sujal-Samai-b6d2c9e8d513441b9c0cc863d69d3705?pvs=4" className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5 rounded-lg hover:text-teal-400 transition-colors duration-300">Notion Resume <span className='animate-pulse'><BsArrowUpRight/></span></Link>
           </aside>
         </div>
       </div>
       <aside className="flex md:hidden flex-col content-center justify-center gap-3 md:gap-5 w-11/12 mx-auto">
             <button className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5 border border-teal-400 rounded-lg hover:text-teal-400 transition-colors duration-300" onClick={()=> window.open('https://docs.google.com/document/d/1BO9Kog7_RBK9es3TXP6lgrhnQ03dJeE131KPui6_vZA/edit?usp=sharing')}>Resume on Drive <span className='animate-pulse'><BsArrowUpRight/></span></button>
             <Link href="/resume.pdf" className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5  border border-teal-400 rounded-lg hover:text-teal-400 transition-colors duration-300">Resume as PDF <span className='animate-pulse'><BsArrowUpRight/></span></Link>
-            <Link href="https://indigo-ring-f16.notion.site/Sujal-Samai-b6d2c9e8d513441b9c0cc863d69d3705?pvs=4" className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5  border border-teal-400 rounded-lg hover:text-teal-400 transition-colors duration-300">Notion Resume <span className='animate-pulse'><BsArrowUpRight/></span></Link>
+            <Link href="https://sujalsamai.notion.site/Sujal-Samai-b6d2c9e8d513441b9c0cc863d69d3705?pvs=4" className="flex items-center justify-center gap-1 bg-stone-950 py-2 px-5  border border-teal-400 rounded-lg hover:text-teal-400 transition-colors duration-300">Notion Resume <span className='animate-pulse'><BsArrowUpRight/></span></Link>
           </aside>
     </>
   )
